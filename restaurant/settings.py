2416d910b4d91c2457c5a28c8cb7594f60385f06
seiwailai <seiwailai9@live.co.uk>
2021-01-17 20:14:32 +0800

    set debug to false

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rump-house-restaurant.herokuapp.com', '127.0.0.1']
 
